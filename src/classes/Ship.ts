5251e8ccef3b0a4c944ea4f06050c657bdb3e1af
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-07-31 12:46:31 -0700

    added validity check for fitting the ship within board

--- src/classes/Ship.ts
+++ src/classes/Ship.ts
@@ -1,6 +1,6 @@
 export default class Ship {
   readonly length: number;
-  positions: Fixed<number> = [];
+  positions: number[] = [];
   #lives: number;
   isDestroyed: boolean = false;
   constructor(length: number, positions: number[]) {
@@ -27,13 +27,14 @@ export default class Ship {
 // should the coords be in the Ship class?
 // There should be a function that inputs the rest of array from one index. Generate it based on the ship length.
 
-type directions = "down" | "right";
-export function positionsFromCoord(direction: directions, coord: number, length: number) {
+type Directions = "down" | "right";
+export function positionsFromCoord(direction: Directions, coord: number, length: number) {
   let pos: number[] = [];
   for (let i = 0; i < length; i++) {
     if (direction === "down") pos.push(coord + 10 * i);
     if (direction === "right") pos.push(coord + 1 * i);
   }
+
   return pos;
 }
 
@@ -58,39 +59,30 @@ export function positionsFromCoord(direction: directions, coord: number, length:
  */
 
 export function checkPositionsIfValid(
-  direction: directions,
+  direction: Directions,
   coord: number,
   length: number
   // grid: string[]
 ) {
-  const adjacentCoords: number[] = [];
-
   let isNotValid = false;
 
-  // determine which direction the ship is facing
+  // check validity of positions by seeing if it fits inside grid.
+  // if it does not, the function returns
+  const coordFit = checkFit(coord, length, direction);
+  if (coordFit === false) return false;
+
+  // determine the ship's location data
   const coordLocation = coordLocationData(coord, length);
 
   if (direction === "right") {
     const rowsToCheck = rowsToCheckIfFacingRight(coord, coordLocation);
-    rowsToCheck.forEach((row) => {});
-    const rowHandler: test = {
-      one: [],
-      two: [],
-      three: [],
-    };
-    for (let i = 0; i < coordLocation.areaLengthToCheck; i++) {
-      // row one (top or middle row)
-      // grid[rowsToCheck[0 + i]] === "s" && (isNotValid = true);
-      // row two ( middle or bottom)
-      // grid[rowsToCheck[1 + i]] === "s" && (isNotValid = true);
-      // row three (bottom) if exist
-      // (rowsToCheck[2] && grid[rowsToCheck[2 + i]] === "s") &&  (isNotValid = true);
-
-      rowHandler.one.push(rowsToCheck[0] + i);
-      rowHandler.two.push(rowsToCheck[1] + i);
-      rowHandler.three.push(rowsToCheck[2] + i);
-    }
-    return rowHandler;
+    rowsToCheck.forEach((row) => {
+      // check each row | column to see if there is already a ship
+      for (let i = 0; i < coordLocation.areaLengthToCheck; i++) {
+        if (grid[row + i] === "s") return (isNotValid = true);
+      }
+    });
+    return rowsToCheck;
   }
 
   // find all coordinates surrounding it
@@ -99,42 +91,57 @@ export function checkPositionsIfValid(
   // then top
 }
 
-interface test {
-  one: number[];
-  two: number[];
-  three: number[];
+export function checkFit(coord: number, length: number, direction: Directions) {
+  if (direction === "down") {
+    const lastCoordDown = (length - 1) * 10 + coord;
+
+    // By rounding it down to single digits, it makes checking for fit much easier because we don't have to check for double digits and it represents the row the last coordinate is in.
+    // ex. 89 -> 8 || 99 -> 9
+    // Because 9 is the last row, if the last coordinate is bigger than 9, then it is invalid. If it is smaller than 9, then it fits inside grid.
+    const roundToRowNumber = Math.floor(lastCoordDown / 10);
+    return 9 - roundToRowNumber >= 0 ? true : false;
+  } else if (direction === "right") {
+    const lastCoordRight = coord + (length - 1);
+    const maxRowCoord = Math.ceil(coord / 10) * 10 - 1;
+    // example: 35
+    // 35 / 10 = 3.5
+    // Math.ceil(3.5) = 4
+    // 4 * 10 - 1 = 39 (finds the last row coordinate)
+    return lastCoordRight <= maxRowCoord ? true : false;
+  }
 }
+
 export const coordLocationData = (coord: number, length: number): CoordLocations => {
-  const isDivisibleByTen = coord % 10 === 0;
+  const isFirstColumn = coord % 10 === 0;
   const isFirstRow = coord < 10;
   const isLastRow = coord > 89;
-  // const isNormal = isLastRow === false && isFirstRow === false && isDivisibleByTen === false;
-  const areaLengthToCheck = isDivisibleByTen ? length + 1 : length + 2;
+  // const isNormal = isLastRow === false && isFirstRow === false && isFirstColumn === false;
+  const areaLengthToCheck = isFirstColumn ? length + 1 : length + 2;
 
   return {
-    isDivisibleByTen,
+    isFirstColumn,
     isFirstRow,
     isLastRow,
     areaLengthToCheck,
   };
 };
 interface CoordLocations {
-  isDivisibleByTen: boolean;
+  isFirstColumn: boolean;
   isFirstRow: boolean;
   isLastRow: boolean;
   areaLengthToCheck: number;
 }
 
-export function rowsToCheckIfFacingRight(coord: number, coordLocation: CoordLocations) {
+export function rowsToCheckIfFacingRight(coord: number, coordLocation: CoordLocations): number[] {
   // if the coordinate......
-  const { isDivisibleByTen, isFirstRow, isLastRow } = coordLocation;
+  const { isFirstColumn, isFirstRow, isLastRow } = coordLocation;
 
-  // if you are 0, check middle and bottom row [FROM CURRENT COLLUM]
-  if (isDivisibleByTen && isFirstRow) return [coord, coord + 10];
-  // if you are 90, check top and middle row [FROM CURRENT COLLUM]
-  if (isDivisibleByTen && isLastRow) return [coord - 10, coord];
-  // if [10, 20, 30, 40, 50 , 60, 70, 80] check top, middle, and bottom row [FROM CURRENT COLLUM]
-  if (isDivisibleByTen) return [coord - 10, coord, coord + 10];
+  // if you are 0, check middle and bottom row [FROM COORD POSITION]
+  if (isFirstColumn && isFirstRow) return [coord, coord + 10];
+  // if you are 90, check top and middle row [FROM COORD POSITION]
+  if (isFirstColumn && isLastRow) return [coord - 10, coord];
+  // if [10, 20, 30, 40, 50 , 60, 70, 80] check top, middle, and bottom row [FROM COORD POSITION]
+  if (isFirstColumn) return [coord - 10, coord, coord + 10];
   // if [1-9], check middle and bottom row [FROM ONE COL LEFT]
   if (isFirstRow) return [coord - 1, coord + 9];
   // if [91 - 99], set to check top and middle row [FROM ONE COL LEFT]
@@ -145,10 +152,30 @@ export function rowsToCheckIfFacingRight(coord: number, coordLocation: CoordLoca
   return [coord - 11, coord - 1, coord + 9];
 }
 
+/**
+ *
+ * When direction is down, it will be the left, middle, and right columns
+ *
+ */
+export function rowsToCheckIfFacingDown(coord: number, coordLocation: CoordLocations): number[] {
+  // if the coordinate......
+  const { isFirstColumn, isFirstRow, isLastRow } = coordLocation;
+  const lastColumn = coord.toString().endsWith("9");
+
+  // if you are 0, check middle and right column [FROM COORD]
+  if (isFirstColumn && isFirstRow) return [coord, coord + 1];
+
+  // if [9, 19, 29,..., 99] check left and current column
+
+  // if it is not adjacent to any border! If it is normal
+  // check left, middle, and right row [FROM ONE ROW UP]
+  return [coord - 11, coord - 10, coord - 9];
+}
+
 export function isNextToRightBorder(coord: number) {}
 
 // check if the length fits the board. If not, do not place
-
+// check when ship borders the right ship. If it does, one less length to check then.
 /**
  *
  *
