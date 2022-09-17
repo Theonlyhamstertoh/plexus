adac22c90d694e9dcd9a9db15e3f34b9f1a9386f
Weibo Zhang <theonlyhamstertoh@gmail.com>
2022-09-17 14:38:53 -0700

    created websocket server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,3 +2,14 @@ import Ship from "./classes/Ship.js";
 import Gameboard from "./classes/Gameboard.js";
 
 console.log(new Gameboard({ boardLength: [10, 10] }));
+
+import UWS from "uWebSockets.js";
+
+const app: UWS.TemplatedApp = UWS.App({});
+app
+  .get("/*", (res, req) => {
+    res.writeStatus("200 OK").writeHeader("IsExample", "Yes").end("Hello there!");
+  })
+  .listen(3001, (listenSocket) => {
+    if (listenSocket) console.log("Listening to port 3001");
+  });
